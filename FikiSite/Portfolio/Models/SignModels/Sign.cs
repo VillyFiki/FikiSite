64b3cc03d0c204859c1fbdd3d3769b2b4281c59f
VillyFiki <maksstelmakh24@gmail.com>
2020-12-08 15:20:00 +0300

    Добавьте файлы проекта.

---
+++ FikiSite/Portfolio/Models/SignModels/Sign.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FikiSite.Models.SignModels
+{
+    public class Sign
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+
+
+        public static List<Sign> Shuffle(List<Sign> list)
+        {
+            Random rand = new Random();
+            List<Sign> listResult = new List<Sign>();
+
+            for (int i = list.Count - 1; i >= 1; i--)
+            {
+                int j = rand.Next(i + 1);
+
+                Sign tmp = list[j];
+                list[j] = list[i];
+                list[i] = tmp;
+            }
+            return list;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# FikiSite
+
+Portfolio + api + VkBot.
+
+Portfolio: in process... 
+API: in process... 
+VkBot in process... frame completed...
+
+More info in: 
+
+VkApi:          FikiSite/VkApi
+
+Portfolio:      FikiSite/Portfolio)
+
+Api:            FikiSite/api/
